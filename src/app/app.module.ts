9e522579eebe7fd9f8368afbaf1b8556f263f027
megahigh5 <kenaik_908@hotmail.com>
2022-08-01 23:37:35 -0500

    Angular14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+//import { AccessRoutingModule } from './access/access-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { StatusComponent } from './status/status.component';
+//import { LoginComponent } from './login/login.component';
+import { AddcontactComponent } from './addcontact/addcontact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    StatusComponent,
+    AddcontactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //LoginComponent
+    //AccessRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
